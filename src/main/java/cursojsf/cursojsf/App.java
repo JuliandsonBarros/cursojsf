76e0efeffe6c1d9798c0bf42ff5d2e5e274136c2
JuliandsonBarros <juliandsonbarros@gmail.com>
2024-04-30 10:03:28 -0300

    Carga inicial

---
+++ src/main/java/cursojsf/cursojsf/App.java
@@ -0,0 +1,13 @@
+package cursojsf.cursojsf;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
